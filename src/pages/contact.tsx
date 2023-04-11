c73a4cca6f66881c7b135728eb32da5f425e542e
Ronniedevloper <ronniedeveloper256@gmail.com>
2023-04-11 12:27:28 +0300

    implemented all routes and applied styles

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,10 @@
-const About = () => {
+const Contact = () => {
     return (
         <div>
-            <h1>About Us</h1>
+            <h1>Contact us</h1>
             <p>Ladies and gentle straight to the main event of the evening the man in the red conner number of fights eight he has lost one and run away from one his aluta the great</p>
         </div>
     );
 }
  
-export default About;
+export default Contact;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
+import Link from "next/link"
 import Footer from "../../comps/Footer"
 import Navbar from "../../comps/Navbar"
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-        <h1>Homepage</h1>
-      <Footer  />
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Ladies and gentle straight to the main event of the evening the man in the red conner number of fights eight he has lost one and run away from one his aluta the great</p>
+      <p className={styles.text}>Ladies and gentle straight to the main event of the evening the man in the red conner number of fights eight he has lost one and run away from one his aluta the great</p>
+      <Link href={"/ninjas"} className={styles.btn}>Ninjas
+        Ninjas
+      </Link>
     </div>
   )
 }
